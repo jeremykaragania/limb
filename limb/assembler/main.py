fb0076428d82f6dc9e1a0c9f944ad3a0e4137eb7
Jeremy Karagania <jeremykaragania@icloud.com>
2023-06-09 13:33:35 +0800

    Add instruction reading

--- limb/assembler/main.py
+++ limb/assembler/main.py
@@ -3,16 +3,24 @@
 import sys
 from collections import namedtuple
 
+instruction = namedtuple("instruction", ["opcode", "data"])
+
 assembler_message = namedtuple("assembler_message", ["file_name", "line_number", "type", "text"])
 
 def assemble(filenames):
   messages = []
   for filename in filenames:
     try:
-      open(filename, 'r')
+      f = list(open(filename, 'r'))
     except FileNotFoundError:
       messages.append(assembler_message(None, None, "Error", f"can't open {filename}"))
       break
+    else:
+      for i in f:
+        i = i.split()
+        opcode = i[0]
+        data = i[1:]
+        i = instruction(opcode.lower(), [j.rstrip(',').lower() for j in data])
   return messages
 
 def main():
