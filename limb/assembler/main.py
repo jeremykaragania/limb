0812852b0afded90dfc3c1e33a4f9fedfa264741
Jeremy Karagania <jeremykaragania@icloud.com>
2023-06-11 17:55:31 +0800

    Add opcode regex patterns

--- limb/assembler/main.py
+++ limb/assembler/main.py
@@ -1,11 +1,14 @@
 #!/usr/bin/env python3
 
+import re
 import sys
 from collections import namedtuple
 
 instruction = namedtuple("instruction", ["opcode", "data"])
 
 assembler_message = namedtuple("assembler_message", ["file_name", "line_number", "type", "text"])
+
+instructions = {}
 
 instruction_conditions = {
   "eq": 0b0000,
@@ -24,6 +27,10 @@ instruction_conditions = {
   "le": 0b1101,
   "al": 0b1110
 }
+
+opcode_re = f"(?P<opcode>{'|'.join(instructions)})?"
+condition_re = f"(?P<condition>{'|'.join(instruction_conditions)})?"
+suffix_re = "(?P<suffix>s)?"
 
 def assemble(filenames):
   messages = []
