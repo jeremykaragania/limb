857354c0d014a7724fd364bb5900b0f8dae1c93f
Jeremy Karagania <jeremykaragania@icloud.com>
2023-06-28 01:07:07 +0800

    Add crude pipeline

--- limb/processor/main.v
+++ limb/processor/main.v
@@ -17,24 +17,42 @@ module random_access_memory(
 
   always @ (posedge clk) begin
     if (rw) begin
-      mem[a] = din;
+      mem[a] <= din;
     end
     else begin
-      dout = mem[a];
+      dout <= mem[a];
     end
   end
 endmodule
 
 module control_unit(
-  clk
-  );
+  clk);
   input clk;
-  reg [31:0] a;
-  reg [31:0] din;
-  wire [31:0] dout;
-  reg rw;
-  random_access_memory ram(.clk(clk), .a(a), .din(din), .dout(dout), .rw(rw));
-  wire [3:0] opcode = dout[25:21];
+  reg [31:0] ram_a;
+  reg [31:0] ram_din;
+  wire [31:0] ram_dout;
+  reg ram_rw;
+  random_access_memory ram(
+    .clk(clk),
+    .a(ram_a),
+    .din(ram_din),
+    .dout(ram_dout),
+    .rw(ram_rw));
+  wire [31:0] instruction = ram_dout;
+  reg [3:0] cond;
+  reg [3:0] rd;
+  reg [3:0] rn;
+  reg [11:0] oprnd2;
+  reg [3:0] alu_opcode;
+  reg [31:0] alu_x;
+  reg [31:0] alu_y;
+  wire [31:0] alu_z;
+  arithmetic_logic_unit alu(
+    .opcode(alu_opcode),
+    .x(alu_x),
+    .y(alu_y),
+    .z(alu_z)
+    );
   reg [31:0] r [0:30];
 
   initial begin
@@ -42,7 +60,40 @@ module control_unit(
   end
 
   always @ (posedge clk) begin
-    a <= r[15];
+    ram_a <= r[15];
+    ram_rw <= 0;
+    alu_opcode <= instruction[25:21];
+    cond <= instruction[31:28];
+    rd <= instruction[15:12];
+    rn <= instruction[19:16];
+    oprnd2 <= instruction[11:0];
+    case (alu_opcode)
+      4'b1101: begin
+        alu_x <= rd;
+        alu_y <= oprnd2;
+      end
+    endcase
+    r[alu_x] <= alu_z;
     r[15] <= r[15] + 1;
+  end
+endmodule
+
+module arithmetic_logic_unit(
+  opcode,
+  x,
+  y,
+  z
+);
+  input [3:0] opcode;
+  input [31:0] x;
+  input [31:0] y;
+  output reg [31:0] z;
+
+  always @ (opcode) begin
+    case (opcode)
+      4'b1101: begin
+        z = y;
+      end
+    endcase
   end
 endmodule
