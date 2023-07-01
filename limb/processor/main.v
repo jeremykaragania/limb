dbabefaa6a9b6eb01e317aa605bb46159eabe15b
Jeremy Karagania <jeremykaragania@icloud.com>
2023-07-01 22:23:21 +0800

    Add condition fields to processor

--- limb/processor/main.v
+++ limb/processor/main.v
@@ -39,39 +39,112 @@ module control_unit(
     .dout(ram_dout),
     .rw(ram_rw));
   wire [31:0] instruction = ram_dout;
-  reg [3:0] cond;
-  reg oprnd2_type;
-  reg [3:0] rd;
-  reg [3:0] rn;
-  reg [11:0] oprnd2;
-  reg [3:0] opcode;
+  wire [3:0] cond = instruction[31:28];
+  wire oprnd2_type = instruction[25];
+  wire [3:0] rd = instruction[15:12];
+  wire [3:0] rn = instruction[19:16];
+  wire [11:0] oprnd2 = instruction[11:0];
+  reg do_execute;
+  wire [3:0] opcode = instruction[25:21];
   reg [31:0] alu_x;
   reg [31:0] alu_y;
   wire [31:0] alu_z;
   arithmetic_logic_unit alu(
+    .do_execute(do_execute),
     .opcode(opcode),
     .x(alu_x),
     .y(alu_y),
     .z(alu_z)
     );
   reg [31:0] r [0:30];
+  reg [31:0] cpsr;
   reg do_writeback;
   reg [31:0] source;
   reg [3:0] destination;
 
   initial begin
     r[15] = 0;
+    cpsr = 0;
   end
 
   always @ (posedge clk) begin
     ram_a <= r[15];
     ram_rw <= 0;
-    opcode <= instruction[25:21];
-    cond <= instruction[31:28];
-    oprnd2_type <= instruction[25];
-    rd <= instruction[15:12];
-    rn <= instruction[19:16];
-    oprnd2 <= instruction[11:0];
+    case (cond)
+      4'b0000: begin
+        if (cpsr[30]) begin
+          do_execute <= 1;
+        end
+      end
+      4'b0001: begin
+        if (!cpsr[30]) begin
+          do_execute <= 1;
+        end
+      end
+      4'b0010: begin
+        if (cpsr[29]) begin
+          do_execute <= 1;
+        end
+      end
+      4'b0011: begin
+        if (!cpsr[29]) begin
+          do_execute <= 1;
+        end
+      end
+      4'b0100: begin
+        if (cpsr[31]) begin
+          do_execute <= 1;
+        end
+      end
+      4'b0101: begin
+        if (!cpsr[31]) begin
+          do_execute <= 1;
+        end
+      end
+      4'b0110: begin
+        if (cpsr[28]) begin
+          do_execute <= 1;
+        end
+      end
+      4'b0111: begin
+        if (!cpsr[28]) begin
+          do_execute <= 1;
+        end
+      end
+      4'b1000: begin
+        if (cpsr[29] || !cpsr[30]) begin
+          do_execute <= 1;
+        end
+      end
+      4'b1001: begin
+        if (!cpsr[29] || cpsr[30]) begin
+          do_execute <= 1;
+        end
+      end
+      4'b1010: begin
+        if (cpsr[31] == cpsr[28]) begin
+          do_execute <= 1;
+        end
+      end
+      4'b1011: begin
+        if (cpsr[31] != cpsr[28]) begin
+          do_execute <= 1;
+        end
+      end
+      4'b1100: begin
+        if (!cpsr[28] || cpsr[31] == cpsr[28]) begin
+          do_execute <= 1;
+        end
+      end
+      4'b1101: begin
+        if (cpsr[28] || cpsr[31] != cpsr[28]) begin
+          do_execute <= 1;
+        end
+      end
+      4'b1110: begin
+        do_execute <= 1;
+      end
+    endcase
     case (opcode)
       4'b1101: begin
         alu_x <= rd;
@@ -89,21 +162,24 @@ module control_unit(
 endmodule
 
 module arithmetic_logic_unit(
+  do_execute,
   opcode,
   x,
   y,
-  z
-);
+  z);
+  input do_execute;
   input [3:0] opcode;
   input [31:0] x;
   input [31:0] y;
   output reg [31:0] z;
 
   always @ (*) begin
-    case (opcode)
-      4'b1101: begin
-        z = y;
-      end
-    endcase
+    if (do_execute) begin
+      case (opcode)
+        4'b1101: begin
+          z = y;
+        end
+      endcase
+    end
   end
 endmodule
