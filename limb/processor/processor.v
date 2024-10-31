2c49b8ab4dfc815286d9612b401f06ce539d54ef
Jeremy Karagania <jeremykaragania@icloud.com>
2024-10-31 16:39:34 +0800

    Add program counter to instruction fetch module

--- limb/processor/processor.v
+++ limb/processor/processor.v
@@ -74,18 +74,28 @@ endmodule
 
 module instruction_fetch (
   input clk,
+  input write_pc_i,
+  input [31:0] pc_i,
   input [31:0] instr_i,
+  output reg [31:0] pc_o,
   output reg [31:0] instr_o);
 
   initial begin
+    pc_o = 32'b0;
     instr_o = 32'b0;
   end
 
   always @ (posedge clk) begin
+    if (write_pc_i) begin
+      pc_o <= pc_i;
+    end
+    else begin
+      pc_o <= pc_o + 32'd1;
+    end
+
     instr_o <= instr_i;
   end
 endmodule
-
 
 module processor (
   input clk,
