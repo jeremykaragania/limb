d93631b489d5ff97167fa60856ad945d8249ceea
Jeremy Karagania <jeremykaragania@icloud.com>
2026-01-18 22:16:41 +0800

    Move ALU to instruction execute module

--- limb/instruction_execute.v
+++ limb/instruction_execute.v
@@ -91,4 +91,123 @@ module instruction_execute (
   end
 endmodule
 
+module arithmetic_logic_unit (
+  input clk,
+  input rst,
+  input [31:0] a,
+  input [31:0] b,
+  input [31:0] cpsr,
+  input [0:3] opcode,
+  output reg [31:0] result);
+
+  always @ (posedge clk) begin
+    if (rst) begin
+      result <= 32'b0;
+    end
+    else begin
+      case (opcode)
+        4'b1101: begin // MOV
+          result <= b;
+        end
+        4'b1111: begin // MVN
+          result <= ~b;
+        end
+        4'b0100: begin // ADD
+          result <= a + b;
+        end
+        4'b0101: begin // ADC
+          result <= a + b + cpsr[29];
+        end
+        4'b0010: begin // SUB
+          result <= a - b;
+        end
+        4'b0110: begin // SBC
+          result <= a - b - cpsr[29];
+        end
+        4'b0011: begin // RSB
+          result <= b - a;
+        end
+        4'b0111: begin // RSC
+          result <= b - a - cpsr[29];
+        end
+        4'b1010: begin // CMP
+          result[31] <= ($unsigned(a) + $unsigned(~b) + $unsigned(1)) >> 32'd31;
+          result[30] <= !(a + ~b + 32'b1);
+          result[29] <= $unsigned((a + ~b + 32'b1) >> 32'b1) != (a + ~b + 32'b1);
+          result[28] <= $signed((a + ~b + 32'b1) >> 32'b1) != ($signed(a) + $signed(~b) + $unsigned(32'b1));
+          result[27:0] <= cpsr[27:0];
+        end
+        4'b1011: begin // CMN
+          result[31] <= ($unsigned(a) + $unsigned(b) + $unsigned(1)) >> 32'd31;
+          result[30] <= !(a + ~b + 32'b1);
+          result[29] <= $unsigned((a + b + 32'b1) >> 32'b1) != (a + b + 32'b1);
+          result[28] <= $signed((a + b + 32'b1) >> 32'b1) != ($signed(a) + $signed(b) + $unsigned(32'b1));
+          result[27:0] <= cpsr[27:0];
+        end
+        4'b1000: begin // TST
+          result[31] <= (a & b) >> 32'd31;
+          result[30] <= !(a & b);
+          result[29:0] <= cpsr[29:0];
+        end
+        4'b1001: begin // TEQ
+          result[31] <= (a ^ b) >> 32'd31;
+          result[30] <= !(a & b);
+          result[29:0] <= cpsr[29:0];
+        end
+        4'b0000: begin // AND
+          result <= a & b;
+        end
+        4'b0001: begin // EOR
+          result <= a ^ b;
+        end
+        4'b1100: begin // ORR
+          result <= a | b;
+        end
+        4'b1110: begin // BIC
+          result <= a & ~b;
+        end
+      endcase
+    end
+  end
+endmodule
+
+module multiplier(
+  input clk,
+  input rst,
+  input [31:0] a,
+  input [31:0] b,
+  input [31:0] c,
+  input [31:0] d,
+  input [2:0] type,
+  output reg [63:0] result);
+
+  always @ (posedge clk) begin
+    if (rst) begin
+      result <= 64'b0;
+    end
+    else begin
+      case (type)
+        3'b000: begin // MUL
+          result <= $signed(a) * $signed(b);
+        end
+        3'b001: begin // MLA
+          result <= $signed(a) * $signed(b) + $signed(c);
+        end
+        3'b100: begin // SMULL
+          result <= $unsigned(a) * $unsigned(b);
+        end
+        3'b101: begin // SMLAL
+          result <= $unsigned(a) * $unsigned(b) + $signed({c, d});
+        end
+        3'b110: begin // UMULL
+          result <= $unsigned(a) * $unsigned(b);
+        end
+        3'b111: begin // UMLAL
+          result <= $unsigned(a) * $unsigned(b) + $unsigned({c, d});
+        end
+      endcase
+    end
+  end
+endmodule
+
 `endif
