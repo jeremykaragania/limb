7582b4784644b77e40275329704bc453255c80d7
Jeremy Karagania <jeremykaragania@icloud.com>
2026-01-21 18:56:34 +0800

    Add missing data processing instruction opcodes

--- tools/assembler.py
+++ tools/assembler.py
@@ -9,22 +9,26 @@ instruction = namedtuple("instruction", ["opcode", "data"])
 assembler_message = namedtuple("assembler_message", ["file_name", "line_number", "type", "text"])
 
 opcode_t = {
-  "mov": 0b1101,
-  "mvn": 0b1111,
-  "add": 0b0100,
-  "adc": 0b0101,
-  "sub": 0b0010,
-  "sbc": 0b0110,
-  "rsb": 0b0011,
-  "rsc": 0b0111,
-  "cmp": 0b1010,
-  "cmn": 0b1011,
-  "tst": 0b1000,
-  "teq": 0b1001,
   "and": 0b0000,
   "eor": 0b0001,
+  "sub": 0b0010,
+  "rsb": 0b0011,
+  "add": 0b0100,
+  "adc": 0b0101,
+  "sbc": 0b0110,
+  "rsc": 0b0111,
+  "tst": 0b1000,
+  "teq": 0b1001,
+  "cmp": 0b1010,
+  "cmn": 0b1011,
   "orr": 0b1100,
+  "mov": 0b1101,
+  "lsl": 0b1101,
+  "asr": 0b1101,
+  "rrx": 0b1101,
+  "ror": 0b1101,
   "bic": 0b1110,
+  "mvn": 0b1111
 }
 
 cond_t = {
