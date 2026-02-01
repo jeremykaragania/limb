35754da0c60cc8679edbed8d96a7aa06581d5639
Jeremy Karagania <jeremykaragania@icloud.com>
2026-02-01 16:31:58 +0800

    Fix micro-operation layout

--- limb/micro_operations.v
+++ limb/micro_operations.v
@@ -2,16 +2,16 @@
 `define MICRO_OPERATIONS_V
 
 // UOP classes.
-`define UOP_BRANCH      4'd1
-`define UOP_INTEGER     4'd2
-`define UOP_INTEGER_M   4'd3
-`define UOP_LOAD        4'd4
-`define UOP_STORE       4'd5
-`define UOP_FP          4'd6
+`define UOP_BRANCH      3'd1
+`define UOP_INTEGER     3'd2
+`define UOP_INTEGER_M   3'd3
+`define UOP_LOAD        3'd4
+`define UOP_STORE       3'd5
+`define UOP_FP          3'd6
 
 // UOP bit positions.
 `define UOP_VALID_B     63
 `define UOP_CLASS_MSB   62
-`define UOP_CLASS_LSB   59
+`define UOP_CLASS_LSB   60
 
 `endif
